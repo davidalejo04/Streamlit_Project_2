5460e33fadd0e383d7b72408d5a731f9a8c53860
davidalejo04 <138809407+davidalejo04@users.noreply.github.com>
2026-01-30 22:14:19 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,4 +1,23 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import numpy as np
 import geopandas as gpd
 import folium as fm
+from groq import Groq
+from sodapy import Socrata
+
+# Unauthenticated client only works with public data sets. Note 'None'
+# in place of application token, and no username or password:
+client = Socrata("www.datos.gov.co", None)
+
+# Example authenticated client (needed for non-public datasets):
+# client = Socrata(www.datos.gov.co,
+#                  MyAppToken,
+#                  username="user@example.com",
+#                  password="AFakePassword")
+
+# First 2000 results, returned as JSON from API / converted to Python list of
+# dictionaries by sodapy.
+results = client.get("vtub-3de2", limit=500000)
